d17a9835bc8a39fdacc3c7ef5bff74dd1fffe7bd
Ben Reilly <benreilly0809@gmail.com>
2022-08-01 22:40:28 -0700

    be reallly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
-// In _document.js
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script src="https://s3.us-west-2.amazonaws.com/dev-scripts.banabo.io/index.js"></script>
+        <title>Ben Reilly</title>
       </Head>
       <body>
         <Main />
